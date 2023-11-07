0cf699aa8c14b96ebf57291de539f5bdae25bd16
Adeline Ndacyayisenga <ndacya.adeline@gmail.com>
2023-11-07 09:56:44 -0500

    handleClick on the Nav buttons and the url matches the browser url

--- my-gallery-app/src/App.jsx
+++ my-gallery-app/src/App.jsx
@@ -19,12 +19,12 @@ function App() {
 
   function fetchData(query) {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-    .then(response => {
-      setPhotos(response.data.photos.photo)
-    })
-    .catch(error => {
-      console.log("Error fetching and parsing data", error);
-    })
+      .then(response => {
+        setPhotos(response.data.photos.photo)
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      })
   }
 
   useEffect(() => fetchData(query), [query])
@@ -37,16 +37,17 @@ function App() {
     <>
       <div className="container">
         <Search changeQuery={handleQueryChange} />
-        <Nav />
-        {console.log(query)}
-        <Routes>
-          <Route path="/" element={<Navigate to="/cats" />} />
-          <Route path="/cats" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
-          <Route path="/dogs" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
-          <Route path="/computers" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
-          <Route path="/search/:query" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
-          <Route path="/*" element={<NoPhotos />} />
-        </Routes>
+        <nav className="main-nav">
+          <Nav changeQuery={handleQueryChange}/>
+          <Routes>
+            <Route path="/" element={<Navigate to="/cats" />} />
+            <Route path="/cats" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
+            <Route path="/dogs" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
+            <Route path="/computers" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
+            <Route path="/search/:query" element={<PhotoList data={photos} pageTitle={`${query} Gifs`} changeQuery={handleQueryChange} />} />
+            <Route path="/*" element={<NoPhotos />} />
+          </Routes>
+        </nav>
       </div>
 
     </>
